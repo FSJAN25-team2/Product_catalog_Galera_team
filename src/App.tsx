9f2e5b273c11a3ed48e66da0490585c5ce92e6cd
kami2693 <kamikaze2693@gmail.com>
2025-04-24 00:33:17 +0200

    feat experiment

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import { Header } from './design/organisms/Header/Header';
 import { Footer } from './design/organisms/Footer/Footer';
 import { GridTemplate } from './design/templates/GridTemplate';
 import { GridRows } from './design/templates/GridRows';
+import { WelcomeAnimation } from './design/organisms/WelcomeAnimation/WelcomeAnimation';
 
-function App() {
+export const App = () => {
   return (
-    <GridRows>
-      <Header />
-
-      <GridTemplate>
-        <Outlet />
-      </GridTemplate>
-
-      <Footer />
-    </GridRows>
+    <>
+      <WelcomeAnimation />
+      <GridRows>
+        <Header />
+        <GridTemplate>
+          <Outlet />
+        </GridTemplate>
+        <Footer />
+      </GridRows>
+    </>
   );
-}
-
-export default App;
+};
