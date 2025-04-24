81fcd80b1750cae26efa74b353f242adb2a6072c
Hleb Bondar <hlebbondar2004@gmail.com>
2025-04-24 14:09:39 +0300

    feat-add-card-to-swiper-phone2

--- src/design/organisms/SwiperBanner/SwiperBanner.tsx
+++ src/design/organisms/SwiperBanner/SwiperBanner.tsx
@@ -18,91 +18,89 @@ const SwiperBanner: React.FC = () => {
   ];
 
   return (
-    <>
-      <div className="banner-outer-container">
-        <div className="slider-container">
-          <button
-            className="custom-nav-button prev-button"
-            aria-label="Previous slide"
-            onClick={() => swiperRef.current?.slidePrev()}
+    <div className="banner-outer-container">
+      <div className="slider-container">
+        <button
+          className="custom-nav-button prev-button"
+          aria-label="Previous slide"
+          onClick={() => swiperRef.current?.slidePrev()}
+        >
+          <svg
+            width="6"
+            height="10"
+            viewBox="0 0 6 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              width="6"
-              height="10"
-              viewBox="0 0 6 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.5 0.75L1.25 5L5.5 9.25"
-                stroke="black"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
+            <path
+              d="M5.5 0.75L1.25 5L5.5 9.25"
+              stroke="black"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
 
-          <div className="swiper-container">
-            <Swiper
-              onSwiper={swiper => {
-                swiperRef.current = swiper;
-              }}
-              pagination={{
-                el: '.custom-pagination',
-                clickable: true,
-                renderBullet: (_: number, className: string) =>
-                  `<span class="${className}"></span>`,
-              }}
-              autoplay={{ delay: 8000, disableOnInteraction: false }}
-              speed={1500}
-              loop={true}
-              modules={[Pagination, Navigation, Autoplay]}
-              className="mySwiper"
-              simulateTouch={false}
-              observer={true}
-              observeParents={true}
-            >
-              {bannerItems.map((item, index) => (
-                <SwiperSlide key={index}>
-                  <Link to={item.path} className="swiper-image-wrapper">
-                    <img
-                      src={`/img/${item.image}.png`}
-                      alt={`Slide ${index + 1}`}
-                      className="slide-image"
-                    />
-                  </Link>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-
-          <button
-            className="custom-nav-button next-button"
-            aria-label="Next slide"
-            onClick={() => swiperRef.current?.slideNext()}
+        <div className="swiper-container">
+          <Swiper
+            onSwiper={swiper => {
+              swiperRef.current = swiper;
+            }}
+            pagination={{
+              el: '.custom-pagination',
+              clickable: true,
+              renderBullet: (_: number, className: string) =>
+                `<span class="${className}"></span>`,
+            }}
+            autoplay={{ delay: 8000, disableOnInteraction: false }}
+            speed={1500}
+            loop={true}
+            modules={[Pagination, Navigation, Autoplay]}
+            className="mySwiper"
+            simulateTouch={false}
+            observer={true}
+            observeParents={true}
           >
-            <svg
-              width="6"
-              height="10"
-              viewBox="0 0 6 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0.5 0.75L4.75 5L0.5 9.25"
-                stroke="black"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
+            {bannerItems.map((item, index) => (
+              <SwiperSlide key={index}>
+                <Link to={item.path} className="swiper-image-wrapper">
+                  <img
+                    src={`/img/${item.image}.png`}
+                    alt={`Slide ${index + 1}`}
+                    className="slide-image"
+                  />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
-        <div className="custom-pagination"></div>
+        <button
+          className="custom-nav-button next-button"
+          aria-label="Next slide"
+          onClick={() => swiperRef.current?.slideNext()}
+        >
+          <svg
+            width="6"
+            height="10"
+            viewBox="0 0 6 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0.5 0.75L4.75 5L0.5 9.25"
+              stroke="black"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
       </div>
-    </>
+
+      <div className="custom-pagination"></div>
+    </div>
   );
 };
 
