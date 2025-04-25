c6bad9d12bb178983ecfb6a9e801d2b3419a9f3d
kami2693 <kamikaze2693@gmail.com>
2025-04-25 13:47:18 +0200

    feat-favourites

--- src/pages/FavouritesPage/FavouritesPage.tsx
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,17 +1,23 @@
 import { ProductCard } from "../../design/organisms/ProductCard/ProductCard";
 import { useAppSelector } from "../../store/hooks"
+import { H1 } from "../../design/atoms/Typography/H1/H1";
+import { P } from "../../design/atoms/Typography/P/P";
+import "./FavouritesPage.scss";
 
 export const FavouritesPage = () => {
   const favourites = useAppSelector(state => state.favouriteProducts);
-  console.log(favourites);
-    return (
-      <>
-      {favourites.map(fav => {
-        return (
-          <ProductCard product={fav} />
-        )
-      })}
-      </>
-        // <h1>Favourites content</h1>
-    )
+  
+  return (
+    <div className="favourites-page container-margin">
+      <H1 className="favourites-page__title">Favourites</H1>
+      <P className="favourites-page__count">{favourites.length} items</P>
+      <div className="global-container">
+        {favourites.map(fav => {
+          return (
+            <ProductCard key={fav.itemId} product={fav} />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
