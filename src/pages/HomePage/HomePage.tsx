f6d16c9fdd7ceccc2c1044e17d7162725ae8100a
vladrlex <v.bondarenko.dev@gmail.com>
2025-04-26 22:38:02 +0300

    feat/create-special-funcs-for sliders

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,42 +1,54 @@
-import './HomePage.scss';
+import { useEffect, useState } from 'react';
+import { getHotPricedProducts, getProducts } from '../../services/api/allProductsAPI';
+import { ShortProduct } from '../../types/ShortProduct';
 import { ProductCard } from '../../design/organisms/ProductCard/ProductCard';
+import { SwiperPhone } from '../../design/organisms/SwiperPhone/SwiperPhone';
 import SwiperBanner from '../../design/organisms/SwiperBanner/SwiperBanner';
 import { ShopByCategory } from '../../design/organisms/ShopByCategory';
 import { H1 } from '../../design/atoms/Typography/H1/H1';
-import { SwiperPhone } from '../../design/organisms/SwiperPhone/SwiperPhone';
-
-const testProduct = {
-  id: 1,
-  color: 'red',
-  year: 2022,
-  name: 'Apple iPhone 14 Pro',
-  fullPrice: 1199,
-  price: 1099,
-  screen: "6.1' OLED",
-  capacity: '256GB',
-  ram: '6GB',
-  image: 'https://placehold.co/600x900',
-  itemId: '1',
-  category: 'phones',
-};
+import { Category } from '../../types/Category';
+import { Sorting } from '../../types/Sorting';
 
 export const HomePage = () => {
-  const productCards = Array.from({ length: 12 }, (_, i) => (
-    <ProductCard key={i} product={testProduct} />
-  ));
+  const [hotProducts, setHotProducts] = useState<ShortProduct[]>([]);
+  const [newProducts, setNewProducts] = useState<ShortProduct[]>([]);
+
+  useEffect(() => {
+    getHotPricedProducts().then(setHotProducts);
+  }, []);
+
+  useEffect(() => {
+    getProducts({
+      limit: 10,
+      page: 1,
+      category: Category.Phones,
+      sortBy: Sorting.Newest,
+    }).then(res => {
+      setNewProducts(res.products);
+    });
+  }, []);
+
   return (
     <>
       <H1 className="home-page__title">Welcome to Nice Gadgets store</H1>
       <SwiperBanner />
 
       <div className="home-page__content">
-        <SwiperPhone title="Brand new models">{productCards}</SwiperPhone>
+        <SwiperPhone title="Brand new models">
+          {newProducts.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </SwiperPhone>
       </div>
 
       <ShopByCategory />
 
       <div className="home-page__content">
-        <SwiperPhone title="Hot prices">{productCards}</SwiperPhone>
+        <SwiperPhone title="Hot prices">
+          {hotProducts.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </SwiperPhone>
       </div>
     </>
   );
--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,3 +1,49 @@
+import { useEffect, useState } from 'react';
+import { getProducts } from '../../services/api/allProductsAPI';
+import { ShortProduct } from '../../types/ShortProduct';
+import { ProductCard } from '../../design/organisms/ProductCard/ProductCard'; // якщо потрібно виводити карточки
+import { Category } from '../../types/Category';
+import { Sorting } from '../../types/Sorting';
+import { SwiperPhone } from '../../design/organisms/SwiperPhone/SwiperPhone';
+import { H1 } from '../../design/atoms/Typography/H1/H1';
+
+function getRandomProducts(
+  products: ShortProduct[],
+  count: number,
+): ShortProduct[] {
+  const shuffled = [...products].sort(() => Math.random() - 0.5);
+  return shuffled.slice(0, count);
+}
+
 export const ItemCard = () => {
-  return (<h1 className="itemclass">Item card content</h1>);
+  const [recommendedProducts, setRecommendedProducts] = useState<
+    ShortProduct[]
+  >([]);
+
+  useEffect(() => {
+    getProducts({
+      limit: 50,
+      page: 1,
+      category: Category.Phones,
+      sortBy: Sorting.Newest,
+    }).then(res => {
+      const products = res.products;
+      const randomProducts = getRandomProducts(products, 8);
+      setRecommendedProducts(randomProducts);
+    });
+  }, []);
+
+  return (
+    <>
+      <H1 className="item-card__title">Item card content</H1>
+
+      <div className="item-card__slider">
+        <SwiperPhone title="You may also like">
+          {recommendedProducts.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </SwiperPhone>
+      </div>
+    </>
+  );
 };
