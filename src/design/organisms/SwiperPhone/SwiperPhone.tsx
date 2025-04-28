60167562a8918ddb588c2d7e993bcf3843032f04
Hleb Bondar <hlebbondar2004@gmail.com>
2025-04-28 16:48:04 +0300

    fix-home-page

--- src/design/organisms/SwiperPhone/SwiperPhone.tsx
+++ src/design/organisms/SwiperPhone/SwiperPhone.tsx
@@ -7,14 +7,19 @@ import { Swiper as SwiperType } from 'swiper';
 
 import './SwiperPhone.scss';
 import { H2 } from '../../atoms/Typography/H2/H2';
+import { SkeletonCards } from '../SkeletonCards/SkeletonCards';
 
 type SwiperPhoneProps = {
   children?: ReactNode;
   title?: string;
+  isLoading?: boolean;
+  skeletonCount?: number;
 };
 
 export const SwiperPhone: React.FC<SwiperPhoneProps> = ({
   children,
+  isLoading = false,
+  skeletonCount = 4,
   title = 'Brand new models',
 }) => {
   const swiperRef = useRef<SwiperType | null>(null);
@@ -41,7 +46,7 @@ export const SwiperPhone: React.FC<SwiperPhoneProps> = ({
                   fillRule="evenodd"
                   clipRule="evenodd"
                   d="M10.47 3.53a.75.75 0 0 1 0 1.06L7.06 8l3.41 3.41a.75.75 0 1 1-1.06 1.06l-4-4a.75.75 0 0 1 0-1.06l4-4a.75.75 0 0 1 1.06 0z"
-                  className='nav-arrow-icon-phones'
+                  className="nav-arrow-icon-phones"
                 />
               </svg>
             </button>
@@ -61,7 +66,7 @@ export const SwiperPhone: React.FC<SwiperPhoneProps> = ({
                   fillRule="evenodd"
                   clipRule="evenodd"
                   d="M5.53 3.53a.75.75 0 0 0 0 1.06L8.94 8l-3.41 3.41a.75.75 0 1 0 1.06 1.06l4-4a.75.75 0 0 0 0-1.06l-4-4a.75.75 0 0 0-1.06 0z"
-                  className='nav-arrow-icon-phones'
+                  className="nav-arrow-icon-phones"
                 />
               </svg>
             </button>
@@ -85,7 +90,7 @@ export const SwiperPhone: React.FC<SwiperPhoneProps> = ({
             watchOverflow={true}
             breakpoints={{
               320: {
-                slidesPerView: 1.2,
+                slidesPerView: 1.4,
                 spaceBetween: 16,
               },
               576: {
@@ -102,7 +107,16 @@ export const SwiperPhone: React.FC<SwiperPhoneProps> = ({
               },
             }}
           >
-            {children ? (
+            {isLoading ? (
+              Array.from({ length: skeletonCount }).map((_, i) => (
+                <SwiperSlide
+                  key={`skeleton-${i}`}
+                  className="product-carousel__slide"
+                >
+                  <SkeletonCards quantity={1} />
+                </SwiperSlide>
+              ))
+            ) : children ? (
               Array.isArray(children) ? (
                 React.Children.map(children, (child, index) => (
                   <SwiperSlide key={index} className="product-carousel__slide">
