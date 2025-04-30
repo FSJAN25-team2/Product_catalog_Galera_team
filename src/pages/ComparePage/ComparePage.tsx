6b9c0dee2fb3be365b5e0560f1e5eaedf4c6333a
Ovramenko Andrii <anovramenko2@gmail.com>
2025-05-01 01:31:09 +0300

    SCRUM-82/finish comparePage

--- src/pages/ComparePage/ComparePage.tsx
+++ src/pages/ComparePage/ComparePage.tsx
@@ -1,15 +1,40 @@
-import { CompareCard } from "../../design/organisms/CompareCard/CompareCard";
-import { useAppSelector } from "../../store/hooks";
+import { ButtonBack } from '../../design/atoms/ButtonBack/ButtonBack';
+import { ComparisonTable } from '../../design/organisms/CompareTable/CompareTable';
+import { useAppSelector } from '../../store/hooks';
 
 export const ComparePage = () => {
-  const compareProducts = useAppSelector(state => state.compareProducts)
+  const compareProducts = useAppSelector(state => state.compareProducts);
+  const comparePhones = compareProducts.filter(
+    cProduct => cProduct.category === 'phones',
+  );
+  const compareTablets = compareProducts.filter(
+    cProduct => cProduct.category === 'tablets',
+  );
+  const compareAccessories = compareProducts.filter(
+    cProduct => cProduct.category === 'accessories',
+  );
+
+  if (!compareProducts || compareProducts.length === 0) {
+    return (
+      <>
+        <ButtonBack className='mb-24'/>
+        <div className="comparison-empty">No items to compare</div>
+      </>
+    );
+  }
+
   return (
     <>
-    {compareProducts.map(product => {
-      return (
-        <CompareCard detailProduct={product} key={product.id}/>
-      )
-    })}
+      <ButtonBack className='mb-24'/>
+      {!!comparePhones.length && (
+        <ComparisonTable compareProducts={comparePhones} />
+      )}
+      {!!compareTablets.length && (
+        <ComparisonTable compareProducts={compareTablets} />
+      )}
+      {!!compareAccessories.length && (
+        <ComparisonTable compareProducts={compareAccessories} />
+      )}
     </>
   );
 };
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { FullProduct } from '../types/FullProduct';
 import { ShortProduct } from '../types/ShortProduct';
+import { mobilesSpecKeys } from './constants';
 
 export const Capitalize = (text: string) => {
   return text.slice(0, 1).toUpperCase() + text.slice(1).toLowerCase();
@@ -9,20 +10,8 @@ type SpecsType = {
   [key: string]: string | string[];
 };
 
-
-const specKeys = [
-  'resolution',
-  'processor',
-  'camera',
-  'zoom',
-  'cell',
-  'screen',
-  'ram',
-  'capacity',
-];
-
 export const getSpecs = (product: ShortProduct | FullProduct) => {
-  return specKeys.reduce((acc: SpecsType, key) => {
+  return mobilesSpecKeys.reduce((acc: SpecsType, key) => {
     if (key in product) {
       acc[key] = product[key as keyof (ShortProduct | FullProduct)] as string | string[];
     }
